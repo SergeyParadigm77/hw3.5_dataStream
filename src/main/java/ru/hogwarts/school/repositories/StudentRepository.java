54ba61da031ea7210c18fd254d3c3a1f2d029bc2
SergeyParadigm77 <paradigm77@rambler.ru>
2023-07-06 22:53:16 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/repositories/StudentRepository.java
@@ -0,0 +1,16 @@
+package ru.hogwarts.school.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    ArrayList<Student> findByAgeBetweenOrderByAge(int min, int max);
+    Collection<Student> findAllByFacultyId(Long id);
+}
